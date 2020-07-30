b66fb80ba59cd6960ee6c1933ace59d49dd40e38
Norbert <norbert@Norberts-MacBook-Pro.local>
2020-07-30 18:40:29 -0500

    initalize server.js

--- server.js
+++ server.js
@@ -17,3 +17,6 @@ mongoose.connect(connectionString, {
 mongoose.connection.on('connected', () => console.log(`Mongoose connected to ${connectionString}`))
 mongoose.connection.on('disconnected', () => console.log('Mongoose disconnected'))
 mongoose.connection.on('error', (err) => console.log('Mongoose error', err))
+
+
+
