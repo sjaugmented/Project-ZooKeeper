0b84eeb178d848441c46f649f8bec6d64fcdcf9d
sjaugmented <sjaugmented@gmail.com>
2020-07-31 16:22:47 -0700

    commented out all of animals.js for testing

--- controllers/animals.js
+++ controllers/animals.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const User = require('../models/Animal');
-const route = express.Router();
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const User = require('../models/Animal');
+// const route = express.Router();
 
-route.post('/', async(req, res) => {
-    const(keyWhatever, keyMore) = req.body;
-    let user = {};
-    user.keyWhatever = keyWhatever;
-    user.keyMore = keyMore;
-    let userModel = new User(user);
-  await userModel.save();
-    // sends data model as a response
-    res.json(userModel);
-});
+// route.post('/', async(req, res) => {
+//     const(keyWhatever, keyMore) = req.body;
+//     let user = {};
+//     user.keyWhatever = keyWhatever;
+//     user.keyMore = keyMore;
+//     let userModel = new User(user);
+//   await userModel.save();
+//     // sends data model as a response
+//     res.json(userModel);
+// });
 
-module.exports = route;
+// module.exports = route;
