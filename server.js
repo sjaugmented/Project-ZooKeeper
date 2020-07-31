7dc8aecac98e7e8e595bf132174089fb0bb98a1d
le-tech80z3 <ka3eem.ali21.971609@gmail.com>
2020-07-31 18:20:49 -0400

    set up controllers and route and server, deleted older models version file

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.static ('./' +'/public'))
 
 connectDB();
+//returning json data
+app.use(express.json({ extended: false }));
+
+app.use('/api/userModel',require('./controllers/Animals'));
 const Port = process.env.Port || 3000;
 
 app.listen(Port, () => console.log('Server started'));
