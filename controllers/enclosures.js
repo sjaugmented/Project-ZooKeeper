63209d4bc67bae5fd1a182b1611b00bfb65f8685
sjaugmented <sjaugmented@gmail.com>
2020-07-31 16:33:14 -0700

    uncommented out routes/enclosures; built skeleton of controllers/enclosures

--- controllers/enclosures.js
+++ controllers/enclosures.js
@@ -5,6 +5,67 @@ const newView = (req, res) => {
     res.render('enclosures/new.ejs')
 }
 
+const create = async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.send('Looks like there was a problem...')
+        console.error(err)
+    }
+}
+
+const index = async (res, req) => {
+    try {
+
+    } catch (err) {
+        res.send('Looks like there was a problem...')
+        console.error(err)
+    }
+}
+
+const show = async (res, req) => {
+    try {
+
+    } catch (err) {
+        res.send('Looks like there was a problem...')
+        console.error(err)
+    }
+}
+
+const deleteData = async (res, req) => {
+    try {
+
+    } catch (err) {
+        res.send('Looks like there was a problem...')
+        console.error(err)
+    }
+}
+
+const edit = async (res, req) => {
+    try {
+
+    } catch (err) {
+        res.send('Looks like there was a problem...')
+        console.error(err)
+    }
+}
+
+const update = async (res, req) => {
+    try {
+
+    } catch (err) {
+        res.send('Looks like there was a problem...')
+        console.error(err)
+    }
+}
+
+
 module.exports = {
-    new: newView
+    new: newView,
+    create,
+    index,
+    show,
+    delete: deleteData,
+    edit,
+    update
 }
