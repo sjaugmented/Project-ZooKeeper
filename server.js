1ec5ce23787270fb0c97e58865225a1e45fd92b1
sjaugmented <sjaugmented@gmail.com>
2020-08-01 13:32:51 -0700

    added methodOverride and mongoose connections to server.js

--- server.js
+++ server.js
@@ -2,12 +2,27 @@ const express = require('express');
 const connectDB = require('./DB/Connection.js')
 const mongoose = require('mongoose')
 const app = express();
+const methodOverride = require('method-override')
+
+// LOCAL DB CONNECTION
+const connectionString = 'mongodb://localhost/zookeeper'
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+
+mongoose.connection.on('connected', () => console.log(`Mongoose connected to ${connectionString}`))
+mongoose.connection.on('disconnected', () => console.log('Mongoose disconnected'))
+mongoose.connection.on('error', (err) => console.log('Mongoose error:', err))
 
 
 
 // middleware
 app.use(express.static('./' + '/public'))
-
+app.use(methodOverride('_method'))
 
 
 connectDB();
