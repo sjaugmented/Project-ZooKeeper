f0f01c8a05b3cb2384f5530b4dc39e8bde881692
sjaugmented <sjaugmented@gmail.com>
2020-08-02 16:43:45 -0700

    built routes and controller paths for sessions

---
+++ controllers/sessions.js
@@ -0,0 +1,20 @@
+const User = require('../models/user')
+
+// ROUTES
+// LOGIN
+const login = (req, res) => {
+    req.session.username = req.body.username
+    req.session.loggedIn = true
+    res.redirect('/')
+}
+
+const logout = async (req, res) => {
+    try {
+        await req.session.destroy()
+    } catch (err) {
+        res.send('Looks like there was a problem...')
+        console.error(err)
+    }
+}
+
+module.exports = { login, logout }
---
+++ routes/sessions.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const sessionsController = require('../controllers/sessions')
+
+
+// ROUTES
+// LOGIN
+router.post('/login', sessionsController.login)
+
+
+// DESTROY
+router.get('/logout', sessionsController.logout)
