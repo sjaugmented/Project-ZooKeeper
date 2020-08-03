25455e19f51847c35268cced66bf654c79223d4e
sjaugmented <sjaugmented@gmail.com>
2020-08-03 11:27:46 -0700

    added user auth to animals controller

--- controllers/animals.js
+++ controllers/animals.js
@@ -4,72 +4,105 @@ const Enclosure = require('../models/enclosure')
 
 
 const newView = (req, res) => {
-    res.render('animals/new.ejs')
+    if (req.session.loggedIn) {
+        res.render('animals/new.ejs', {
+            user: req.session
+        })
+    } else {
+        res.redirect('/')
+    }
 }
 
 const create = async (req, res) => {
-    try {
-        await Animal.create(req.body)
-        res.redirect('/animals')
-    } catch (err) {
-        res.send('Looks like something went wrong...')
-        console.log(err)
+    if (req.session.loggedIn) {
+        try {
+            await Animal.create(req.body)
+            res.redirect('/animals')
+        } catch (err) {
+            res.send('Looks like something went wrong...')
+            console.log(err)
+        }
+    } else {
+        res.redirect('/')
     }
 }
 
 const index = async (req, res) => {
-    try {
-        const foundAnimals = await Animal.find({})
-        res.render('animals/index.ejs', {
-            animals: foundAnimals
-        })
-    } catch (err) {
-        res.send('Looks like something went wrong...')
-        console.log(err)
+    if (req.session.loggedIn) {
+        try {
+            const foundAnimals = await Animal.find({})
+            res.render('animals/index.ejs', {
+                animals: foundAnimals,
+                user: req.session
+            })
+        } catch (err) {
+            res.send('Looks like something went wrong...')
+            console.log(err)
+        }
+    } else {
+        res.redirect('/')
     }
 }
 
 const show = async (req, res) => {
-    try {
-        const foundAnimal = await Animal.findById(req.params.id)
-        res.render('animals/show.ejs', {
-            animal: foundAnimal
-        })
-    } catch (err) {
-        res.send('Looks like something went wrong...')
-        console.log(err)
+    if (req.session.loggedIn) {
+        try {
+            const foundAnimal = await Animal.findById(req.params.id)
+            res.render('animals/show.ejs', {
+                animal: foundAnimal,
+                user: req.session
+            })
+        } catch (err) {
+            res.send('Looks like something went wrong...')
+            console.log(err)
+        }
+    } else {
+        res.redirect('/')
     }
 }
 
 const deleteData = async (req, res) => {
-    try {
-        await Animal.findByIdAndDelete(req.params.id)
-        res.redirect('/animals')
-    } catch (err) {
-        res.send('Looks like something went wrong...')
-        console.log(err)
+    if (req.session.loggedIn) {
+        try {
+            await Animal.findByIdAndDelete(req.params.id)
+            res.redirect('/animals')
+        } catch (err) {
+            res.send('Looks like something went wrong...')
+            console.log(err)
+        }
+    } else {
+        res.redirect('/')
     }
 }
 
 const edit = async (req, res) => {
-    try {
-        const foundAnimal = await Animal.findById(req.params.id)
-        res.render('animals/edit.ejs', {
-            animal: foundAnimal
-        })
-    } catch (err) {
-        res.send('Looks like something went wrong...')
-        console.log(err)
+    if (req.session.loggedIn) {
+        try {
+            const foundAnimal = await Animal.findById(req.params.id)
+            res.render('animals/edit.ejs', {
+                animal: foundAnimal,
+                user: req.session
+            })
+        } catch (err) {
+            res.send('Looks like something went wrong...')
+            console.log(err)
+        }
+    } else {
+        res.redirect('/')
     }
 }
 
 const update = async (req, res) => {
-    try {
-        await Animal.findByIdAndUpdate(req.params.id, req.body)
-        res.redirect('/animals');
-    } catch (err) {
-        res.send('Looks like something went wrong...')
-        console.log(err)
+    if (req.session.loggedIn) {
+        try {
+            await Animal.findByIdAndUpdate(req.params.id, req.body)
+            res.redirect('/animals')
+        } catch (err) {
+            res.send('Looks like something went wrong...')
+            console.log(err)
+        }
+    } else {
+        res.redirect('/')
     }
 }
 
