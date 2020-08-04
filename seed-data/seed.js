6b1a696b747e94c0642a285a0be772ac805fdbc5
sjaugmented <sjaugmented@gmail.com>
2020-08-04 11:33:20 -0700

    beginning seed data

---
+++ seed-data/seed.js
@@ -0,0 +1,74 @@
+const data = require('../models')
+
+const enclosures_list = [
+    {
+        "name": "Reptiles",
+        "keeper": "Dianna",
+        "lastChecked": "",
+        "animals": [],
+        "comments": "",
+        "img": ""
+    }, {
+        "name": "Elephants",
+        "keeper": "Data",
+        "lastChecked": "",
+        "animals": [],
+        "comments": "",
+        "img": ""
+    }, {
+        "name": "Primates",
+        "keeper": "Jean-Luc",
+        "lastChecked": "",
+        "animals": [],
+        "comments": "",
+        "img": ""
+    }, {
+        "name": "Cats",
+        "keeper": "Beverly",
+        "lastChecked": "",
+        "animals": [],
+        "comments": "",
+        "img": ""
+    }, {
+        "name": "Birds",
+        "keeper": "Will",
+        "lastChecked": "",
+        "animals": [],
+        "comments": "",
+        "img": ""
+    }, {
+        "name": "Oceans",
+        "keeper": "Geordi",
+        "lastChecked": "",
+        "animals": [],
+        "comments": "",
+        "img": ""
+    }, {
+        "name": "Plains",
+        "keeper": "",
+        "lastChecked": "",
+        "animals": [],
+        "comments": "",
+        "img": ""
+    }, {
+        "name": "Bears",
+        "keeper": "Worf",
+        "lastChecked": "",
+        "animals": [],
+        "comments": "",
+        "img": ""
+    }, {
+        "name": "Insects",
+        "keeper": "Wesley",
+        "lastChecked": "",
+        "animals": [],
+        "comments": "",
+        "img": ""
+    }
+]
+
+
+
+const animals_list = [
+
+]
