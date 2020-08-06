c96fad39ddfd89e965f72908f9d4dd6d3e875687
sjaugmented <sjaugmented@gmail.com>
2020-08-06 14:55:43 -0700

    updated submaster

--- models/index.js
+++ models/index.js
@@ -4,11 +4,7 @@ const mongoose = require('mongoose')
 // LOCAL DB CONNECTION
 const connectionString = 'mongodb://localhost/zookeeper'
 // ATLAS DB CONNECTION
-<<<<<<< HEAD
-// const connectionString = process.env.DATABASE_URL
-=======
 //const connectionString = process.env.DATABASE_URL
->>>>>>> submaster
 
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
