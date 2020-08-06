cf901d0cf1eb87f7017a9196ffbbcde790c20ed2
sjaugmented <sjaugmented@gmail.com>
2020-08-06 13:47:36 -0700

    updated user model, passport.js, session routes for passport - I think?

--- routes/sessions.js
+++ routes/sessions.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const router = express.Router()
 const sessionsController = require('../controllers/sessions')
+const passport = require('passport')
 
 
 // ROUTES
 // LOGIN
-router.post('/login', sessionsController.login)
+//router.post('/login', sessionsController.login)
+router.get('/auth/google', passport.authenticate(
+    'google',
+    {scope: ['profile', 'email']}
+))
 
+router.get('/oauth2callback', passport.authenticate(
+    {
+        successRedirect: '/',
+        failureRedirect: '/'
+    }
+))
 
 // DESTROY
-router.get('/logout', sessionsController.logout)
+//router.get('/logout', sessionsController.logout)
+router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
 
 module.exports = router
